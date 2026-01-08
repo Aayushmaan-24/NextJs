94cadc4dca6768738a0ab7f92f1812e0ed1e238c
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-01-08 10:45:56 +0530

    Client - Server side rendering

---
+++ app/components/Hello.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const Hello = () => {
+    console.log("This is on client side");
+  return (
+    <></>
+  )
+}
+
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hello from './components/Hello';
 
 const page = () => {
+  console.log("This is on server side");
   return (
-    <div className='text-5xl underline'>Begin with Next js</div>
+    <main>
+      <div className='text-5xl underline'>Begin with Next js</div>
+      <Hello />
+    </main>
   )
 }
 
